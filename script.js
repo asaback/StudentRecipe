2b28dd8d768542f88d30d7ea28cfc40a84f0fb1f
Åsa B <backaasa@gmail.com>
2019-10-08 13:15:35 +0200

    shorter js and centered

--- script.js
+++ script.js
@@ -1,101 +1,49 @@
-var tacoGratin = '<a href="https://www.undertian.com/recept/tacogratang-med-majstopping/">TACO GRATIN</a>';
-var kaleSalad = '<a href="https://undertian.com/recept/gronkalssallad-med-dill-och-kikartor/">KALE SALAD <br> WITH DILL</a>';
-var zucchiniPasta = '<a href="https://www.undertian.com/recept/kramig-zucchini-och-svamppasta/">ZUCCHINI AND <br> MUSHROOM PASTA</a>';
-var broccoliSoup = '<a href= "https://www.undertian.com/recept/billig-broccolisoppa-med-picklad-rodlok/">BROCCOLI SOUP</a>';
-var vegetarianLasagne = '<a href="https://www.undertian.com/recept/billig-vegetarisk-lasagne/">VEGETARIAN <br> LASANGE</a>';
-var tomatoPasta = '<a href= "https://www.undertian.com/recept/snabb-tomatpasta-med-rostade-kikartor/">TOMATO PASTA</a>';
-var veganBeet = '<a href= "https://www.undertian.com/recept/veganska-rodbetsbiffar/">VEGAN BEET <br> STEAKS</a>';
+// var tacoGratin = '<a href="https://www.undertian.com/recept/tacogratang-med-majstopping/">TACO GRATIN</a>';
+// var kaleSalad = '<a href="https://undertian.com/recept/gronkalssallad-med-dill-och-kikartor/">KALE SALAD <br> WITH DILL</a>';
+// var zucchiniPasta = '<a href="https://www.undertian.com/recept/kramig-zucchini-och-svamppasta/">ZUCCHINI AND <br> MUSHROOM PASTA</a>';
+// var broccoliSoup = '<a href= "https://www.undertian.com/recept/billig-broccolisoppa-med-picklad-rodlok/">BROCCOLI SOUP</a>';
+// var vegetarianLasagne = '<a href="https://www.undertian.com/recept/billig-vegetarisk-lasagne/">VEGETARIAN <br> LASANGE</a>';
+// var tomatoPasta = '<a href= "https://www.undertian.com/recept/snabb-tomatpasta-med-rostade-kikartor/">TOMATO PASTA</a>';
+// var veganBeet = '<a href= "https://www.undertian.com/recept/veganska-rodbetsbiffar/">VEGAN BEET <br> STEAKS</a>';
 
 
 var randomAnswers= [
-    tacoGratin,
-   
-    kaleSalad,
-
-    zucchiniPasta,
-
-    broccoliSoup,
-    
-    vegetarianLasagne,
-    
-    tomatoPasta,
-    
-    veganBeet,
-    
-    ];
+    {
+        name: 'TACO GRATIN',
+        link: 'https://www.undertian.com/recept/tacogratang-med-majstopping/',
+        image: 'tacgra.png',
+    },{
+        name: 'KALE SALAD <br> WITH DILL',
+        link: 'https://undertian.com/recept/gronkalssallad-med-dill-och-kikartor/',
+        image: 'kalsal.png',
+    }, {
+        name: 'ZUCCHINI AND <br> MUSHROOM PASTA',
+        link: 'https://www.undertian.com/recept/kramig-zucchini-och-svamppasta/',
+        image: 'crepas.png',
+    }, {
+        name: 'BROCCOLI SOUP',
+        link: 'https://www.undertian.com/recept/billig-broccolisoppa-med-picklad-rodlok/',
+        image: 'brosou.png',
+    },{
+        name: 'VEGETARIAN <br> LASANGE',
+        link: 'https://www.undertian.com/recept/billig-vegetarisk-lasagne/',
+        image: 'veglas.png',
+    },{
+        name: 'TOMATO PASTA',
+        link: 'https://www.undertian.com/recept/snabb-tomatpasta-med-rostade-kikartor/',
+        image: 'tompas.png',
+    },{
+        name: 'VEGAN BEET <br> STEAKS',
+        link: 'https://www.undertian.com/recept/veganska-rodbetsbiffar/',
+        image: 'vegbee.png',
+    }
+  
+];
     
      document.getElementById('button').onclick=function() {
         var randomAnswer = randomAnswers[Math.floor(Math.random()*randomAnswers.length)];
-        document.getElementById('display').innerHTML= randomAnswer;
-
-        if (randomAnswer=== vegetarianLasagne) {
-            document.getElementById('vegetarianLasagne').style.display = "block";
-            document.getElementById('kaleSalad').style.display = "none";
-            document.getElementById('tacoGratin').style.display = "none";
-            document.getElementById('creamyPasta').style.display = "none";
-            document.getElementById('broccoliSoup').style.display = "none";  
-            document.getElementById('tomatoPasta').style.display = "none";
-            document.getElementById('veganBeet').style.display = "none";
-        }
-
-        else if (randomAnswer=== kaleSalad) {
-            document.getElementById('kaleSalad').style.display = "block";
-            document.getElementById('vegetarianLasagne').style.display = "none";
-            document.getElementById('tacoGratin').style.display = "none";
-            document.getElementById('creamyPasta').style.display = "none";
-            document.getElementById('broccoliSoup').style.display = "none";  
-            document.getElementById('tomatoPasta').style.display = "none";
-            document.getElementById('veganBeet').style.display = "none";
-        }
-
-        else if (randomAnswer=== tacoGratin) {
-            document.getElementById('tacoGratin').style.display = "block";
-            document.getElementById('kaleSalad').style.display = "none";
-            document.getElementById('vegetarianLasagne').style.display = "none";
-            document.getElementById('creamyPasta').style.display = "none";
-            document.getElementById('broccoliSoup').style.display = "none"; 
-            document.getElementById('tomatoPasta').style.display = "none";
-            document.getElementById('veganBeet').style.display = "none"; 
-        }
-
-        else if (randomAnswer=== zucchiniPasta) {
-            document.getElementById('creamyPasta').style.display = "block";    
-            document.getElementById('tacoGratin').style.display = "none";
-            document.getElementById('kaleSalad').style.display = "none";
-            document.getElementById('vegetarianLasagne').style.display = "none";
-            document.getElementById('broccoliSoup').style.display = "none";
-            document.getElementById('tomatoPasta').style.display = "none";
-            document.getElementById('veganBeet').style.display = "none";
-        }
-
-        else if (randomAnswer=== broccoliSoup) {
-            document.getElementById('broccoliSoup').style.display = "block"; 
-            document.getElementById('creamyPasta').style.display = "none";    
-            document.getElementById('tacoGratin').style.display = "none";
-            document.getElementById('kaleSalad').style.display = "none";
-            document.getElementById('vegetarianLasagne').style.display = "none";
-            document.getElementById('tomatoPasta').style.display = "none";
-            document.getElementById('veganBeet').style.display = "none";
-        }
-
-        else if (randomAnswer=== tomatoPasta) {
-            document.getElementById('tomatoPasta').style.display = "block";
-            document.getElementById('broccoliSoup').style.display = "none"; 
-            document.getElementById('creamyPasta').style.display = "none";    
-            document.getElementById('tacoGratin').style.display = "none";
-            document.getElementById('kaleSalad').style.display = "none";
-            document.getElementById('vegetarianLasagne').style.display = "none";
-            document.getElementById('veganBeet').style.display = "none";
-        }
-
-        else if (randomAnswer=== veganBeet) {
-            document.getElementById('veganBeet').style.display = "block";
-            document.getElementById('tomatoPasta').style.display = "none";
-            document.getElementById('broccoliSoup').style.display = "none"; 
-            document.getElementById('creamyPasta').style.display = "none";    
-            document.getElementById('tacoGratin').style.display = "none";
-            document.getElementById('kaleSalad').style.display = "none";
-            document.getElementById('vegetarianLasagne').style.display = "none";
-        }
+        document.getElementById('display').innerHTML= randomAnswer.name;
+        document.getElementById('display').href = randomAnswer.link;
+        document.getElementById('recipeImage').src = randomAnswer.image;
 
 }
